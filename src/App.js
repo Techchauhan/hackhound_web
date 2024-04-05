01641323749f5f6bfcf0facf39bf241bd5f5343a
Amitmavi <amitkumar203536@gmail.com>
2024-04-05 21:19:20 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
 
+import "./App.css";
+import AppFooter from "./Components/AppFooter";
+import AppHeader from "./Components/AppHeader";
+import PageContent from "./component/PageContent/index";
+import SideMenu from "./component/SideMenu/index";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppHeader />
+      <div className="SideMenuAndPageContent">
+        <SideMenu></SideMenu>
+        <PageContent></PageContent>
+      </div>
+      <AppFooter />
     </div>
   );
 }
-
 export default App;
---
+++ src/component/AppHeader/index.js
@@ -0,0 +1,81 @@
+import { BellFilled, MailOutlined } from "@ant-design/icons";
+import { Badge, Drawer, List, Space, Typography } from "antd";
+import { useEffect, useState } from "react";
+import { getComments, getOrders } from "../../API";
+
+function AppHeader() {
+  const [comments, setComments] = useState([]);
+  const [orders, setOrders] = useState([]);
+  const [commentsOpen, setCommentsOpen] = useState(false);
+  const [notificationsOpen, setNotificationsOpen] = useState(false);
+
+  useEffect(() => {
+    getComments().then((res) => {
+      setComments(res.comments);
+    });
+    getOrders().then((res) => {
+      setOrders(res.products);
+    });
+  }, []);
+
+  return (
+    <div className="AppHeader">
+     
+    <div ><Typography.Title>Inral Dashboard</Typography.Title></div>
+      <Space>
+        <Badge count={comments.length} dot>
+          <MailOutlined
+            style={{ fontSize: 24 }}
+            onClick={() => {
+              setCommentsOpen(true);
+            }}
+          />
+        </Badge>
+        <Badge count={orders.length}>
+          <BellFilled
+            style={{ fontSize: 24 }}
+            onClick={() => {
+              setNotificationsOpen(true);
+            }}
+          />
+        </Badge>
+      </Space>
+      <Drawer
+        title="Comments"
+        open={commentsOpen}
+        onClose={() => {
+          setCommentsOpen(false);
+        }}
+        maskClosable
+      >
+        <List
+          dataSource={comments}
+          renderItem={(item) => {
+            return <List.Item>{item.body}</List.Item>;
+          }}
+        ></List>
+      </Drawer>
+      <Drawer
+        title="Notifications"
+        open={notificationsOpen}
+        onClose={() => {
+          setNotificationsOpen(false);
+        }}
+        maskClosable
+      >
+        <List
+          dataSource={orders}
+          renderItem={(item) => {
+            return (
+              <List.Item>
+                <Typography.Text strong>{item.title}</Typography.Text> has been
+                ordered!
+              </List.Item>
+            );
+          }}
+        ></List>
+      </Drawer>
+    </div>
+  );
+}
+export default AppHeader;
